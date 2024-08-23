5b3310ee9d263a651b1752cbd5affb4fbc312805
EDUARDO HENZO FREITAS DOS SANTOS <eduardo_hf_santos@estudante.sesisenai.org.br>
2024-08-23 17:17:32 -0300

    criação do primeiro projeto com REACT bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>dinossauros</h1>
+        <h2>carnívoros</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, porro! Odio, nemo sunt, saepe mollitia magni eaque id possimus tempora aut dolores necessitatibus at expedita, officia velit quam! Nihil, ipsum!</p>
+    </>
+  )
+}
+
+export default App
