ad0209e2c01ea694e30708a6700bef3d44be6d49
eduardos4antos <eduardo_hf_santos@estudante.sesisenai.org.br>
2024-08-29 17:21:32 -0300

    add criação de componentes e mais umas coisas ai

---
+++ componentes/src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>atalhos do VS code</h1>
+      <h2>coisas legais para facilotar minha vida </h2>
+      <p>comando para o emet da ul: ul>li*6</p>
+      <ul>
+        <li>atalho: alt + z para quebra automatica de linha. </li>
+        <li>atalho: alt + clique esquerdo para adicionar novoa cursores na tela  </li>
+        <li>atalho: ctrl + shift + k para excluir uma linha inteira </li>
+        <li>atalho: ctrl + ; para comentar ou descomentar </li>
+        <li>atalho: windows + . bare o teclado de emojis </li>
+        <li>atalho: alt + 🔼 para mover uma linha/ bloco </li>
+        <li>atalho: alt + shift + 🔼 copia a linha atual ou bloco</li>
+        <li>atalho: ctrl + j ou ctrl + abre/fecha o terminal</li>
+      </ul>
+      <p>clicando com a rodinha do mouse eu consigo fazer com que varios itens selecionados possam ser escritos de uma só vez</p>
+
+      <p>extensões</p>
+      <p>observação: abaixar pelo VS code</p>
+      <ul>
+        <li>extensões: ES7 React/Redux/GraphQL/React-Native snippets </li>
+        <li>extensões: Auto Rename Tag </li>
+        <li>extensões: color highlight </li>
+        <li>extensões: power mode </li>
+        <li>extensões: VSCode pets </li>
+      </ul>
+
+    </>
+  )
+}
+
+export default App
---
+++ componentesDeVerdade/src/components/Cabecalho.jsx
@@ -0,0 +1,10 @@
+import'./cabecalho.css'
+function Cabecalho() {
+  return (
+    <div className="cabecalho-container">
+      <h1>treinar dragão</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
