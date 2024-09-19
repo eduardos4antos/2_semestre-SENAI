af30c9767b01f9e19eac4ff42ea1c279dd899b0f
EDUARDO HENZO FREITAS DOS SANTOS <eduardo_hf_santos@estudante.sesisenai.org.br>
2024-09-19 17:08:43 -0300

    novas atividades realizadas pelo nosso kratos

--- exercicios-2/src/App.jsx
+++ exercicios-2/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import DiasSemana from './components/diasSemana'
+import Garcom from './components/Garcom'
 
 function App() {
 
   return (
     <>
     <DiasSemana />
+    <Garcom />
     </>
   )
 }
